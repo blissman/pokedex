c6ec836ed04a037cf4827c38dbb54986b3540682
blissman <blissman@gmail.com>
2016-04-01 23:58:15 -0400

    limit index to name and image

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,9 +1,21 @@
 class PokemonsController < ApplicationController
+
   def index
     @pokemons = Pokemon.all
+
+    respond_to do |format|
+      format.html {}
+      format.json {render json: @pokemons, only: ["name", "image_url"]}
+    end
   end
 
   def show
     @pokemon = Pokemon.find(params[:id])
+
+    respond_to do |format|
+      format.html {}
+      format.json {render json: @pokemon}
+    end
   end
+
 end
